b546af39bb4d072fd32a52cdce759c5c9d0b4c74
Pardeep257 <workforpardeep@gmail.com>
2026-07-19 16:13:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.CONTROL_TOWER_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbwHKgTbvjO3kMWQHajJex7cLNcwasic5VJ2-tzfTngmM46QTChDNDNw4emDLPF8xl_cyw/exec",
+  REFRESH_SECONDS: 60,
+  HISTORY_LIMIT: 30
+};
