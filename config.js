497583f9d19b60481c654fc329c2d71baa1d257d
Pardeep257 <workforpardeep@gmail.com>
2026-07-19 20:03:35 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.CONTROL_TOWER_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycby98bh-89odTzkmRu2uUF4JAM8wlY_d_mTCC0ijeLPluDtekYkcDRR8HuqmcK5guYU/exec",
-  REFRESH_SECONDS: 60,
-  HISTORY_LIMIT: 30
-};
