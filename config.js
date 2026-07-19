a822c47e870a67626e7991113a0bbb963f81f233
Pardeep257 <workforpardeep@gmail.com>
2026-07-19 17:27:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CONTROL_TOWER_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwHKgTbvjO3kMWQHajJex7cLNcwasic5VJ2-tzfTngmM46QTChDNDNw4emDLPF8xl_cyw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycby98bh-89odTzkmRu2uUF4JAM8wlY_d_mTCC0ijeLPluDtekYkcDRR8HuqmcK5guYU/exec",
   REFRESH_SECONDS: 60,
   HISTORY_LIMIT: 30
 };
