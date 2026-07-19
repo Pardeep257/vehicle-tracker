737fa64cb7ca86d977df29208eccd5bbb6249143
Pardeep257 <workforpardeep@gmail.com>
2026-07-19 20:33:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.RJK_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycby98bh-89odTzkmRu2uUF4JAM8w1Y_d_mTCC0ijeLPluDtekYkcDRR8Huqmck5GgYUY/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycby98bh-89odTzkmRu2uUF4JAM8wlY_d_mTCC0ijeLPluDtekYkcDRR8HuqmcK5guYU/exec",
   FALLBACK_REFRESH_SECONDS: 10,
-  APP_VERSION: "2.0.0"
+  APP_VERSION: "2.0.1"
 };
